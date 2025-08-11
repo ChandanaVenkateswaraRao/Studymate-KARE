188a2f5f446fa02f787cccf0db4bb610b509cae0
Venkey <venkatchandana2113@gmail.com>
2025-08-11 22:16:04 +0530

    second commit

--- pdf/src/data.js
+++ pdf/src/data.js
@@ -5,20 +5,20 @@ export const subjects = [
       {
         title: "Unit1",
         files: [
-          { name: "Time & Space Complexity", file: "/chapter1/DAA.pdf" },
-          { name: "Algorithm", file: "/chapter1/notes 1.pdf" },
-          { name: "Recurrence_relation", file: "/chapter1/Recurrence_realation.pdf" }
+          { name: "Time & Space Complexity", file: "/chapter1/daa.pdf" },
+          { name: "Algorithm", file: "/chapter1/notes.pdf" },
+          { name: "Recurrence_relation", file: "/chapter1/recurrence_realation.pdf" }
         ]
       },
       {
         title: "Unit2",
             files: [
-    { name: "Brute Force", file: "/chapter2/Brute Force.pdf" },
-    { name: "Dijkstra Algo", file: "/chapter2/Dijkstra Algo.pdf" },
-    { name: "Huffman Enconding", file: "/chapter2/Huffman Enconding.pdf" },
-    { name: "Kurutsuba", file: "/chapter2/Kurutsuba.pdf" },
-    { name: "OBST", file: "/chapter2/OBST.pdf" },
-    { name: "strassen matrix", file: "/chapter2/strassen matrix.pdf" }
+    { name: "Brute Force", file: "/chapter2/bruteforce.pdf" },
+    { name: "Dijkstra Algo", file: "/chapter2/dijkstraalgo.pdf" },
+    { name: "Huffman Enconding", file: "/chapter2/huffmanenconding.pdf" },
+    { name: "Kurutsuba", file: "/chapter2/kurutsuba.pdf" },
+    { name: "OBST", file: "/chapter2/obst.pdf" },
+    { name: "strassen matrix", file: "/chapter2/strassenmatrix.pdf" }
     ]
       },
     //   {
